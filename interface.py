7390960e91d41dfc4395e2287eb16b5861a74164
Val-PC <valentinewilson@gmail.com>
2020-04-05 21:34:50 +0100

    Added basic interface. Testing number of nodes

---
+++ interface.py
@@ -0,0 +1,12 @@
+from PIL import Image, ImageDraw
+
+
+im = Image.new(mode="1", size=(256, 256), color=1)
+im.save(fp="im.png")
+
+
+def add_node(count: int):
+    draw = ImageDraw.Draw(im)
+    xy = ((count - 1) * 30, (count - 1) * 30, count * 30, count * 30)
+    draw.ellipse(xy, None, outline=0)
+    return im
