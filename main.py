feac227a74b1787b9129ee4f9c1dad593629c243
Val-PC <valentinewilson@gmail.com>
2020-04-07 03:10:30 +0100

    Test environment for project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from graph import Graph
 
-graph = Graph(5)  # INITIALISE THE GRAPH SIZE HERE
+graph = Graph(10)  # INITIALISE THE GRAPH SIZE HERE
 
 graph.add_path(1, 2)
 graph.add_path(1, 4)
@@ -13,3 +13,5 @@ graph.analysis()
 graph.search(3)  # EDIT THE STARTING NODE HERE!
 
 print(graph)
+print(graph.node_count)
+print(graph.get_node(str(3)).position)
