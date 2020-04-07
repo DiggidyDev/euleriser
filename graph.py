f86caef1d82bc7f026707942416356e3ef4f0dda
Val-PC <valentinewilson@gmail.com>
2020-04-07 03:09:38 +0100

    Cleaned up graph functionality, added basic graph interface

--- graph.py
+++ graph.py
@@ -1,21 +1,49 @@
-import interface
+from interface import Interface
+from node import Node
 
 
 class Graph:
 
     def __init__(self, nodes: int = None):
         self.current_node = 1
-        self.nodes = nodes
+        self.nodes = []
         self.odd_nodes = []
         self.paths = {k + 1: [] for k in range(nodes)}
+        self.path_count = 0
         self.previous_node = None
         self.travelled = {k + 1: [] for k in range(nodes)}
 
+        for i in range(nodes):
+            node = Node(str(i + 1))
+            self.nodes.append(node)
+
+    @property
+    def node_count(self):
+        return len(self.nodes)
+
+    def __len__(self):
+        """
+        Returns the number of paths in a graph.
+
+        :return:
+        """
+        return self.path_count
+
     def __str__(self):
-        for count in range(self.nodes + 1):
-            image = interface.add_node(count)
+        """
+        Shows graph in image when printed.
+        Used to visualise the nodes and paths.
+
+        :return:
+        """
+        gui = Interface(self.node_count)
+
+        for count in range(self.node_count + 1):
+            image = gui.draw_node(count)
+        else:
+            image = gui.draw_node(1)
         image.show()
-        return ""
+        return "Loading..."
 
     def _dfs(self, start, solution: list):
         """
@@ -51,13 +79,17 @@ class Graph:
         :return:
         """
 
-        if not (0 < start <= self.nodes) or not (0 < start <= self.nodes):
+        if not (0 < start <= self.node_count) or not (0 < start <= self.node_count):
             raise IndexError(f"Please provide valid values between the lower and upper bounds, inclusively: (1-{self.nodes})")
+
+
 
         try:
             self.paths[start].append(end)
         except KeyError:
             self.paths[start] = [end]
+        finally:
+            self.path_count += 1
 
         try:
             self.paths[end].append(start)
@@ -67,7 +99,7 @@ class Graph:
     def analysis(self):
         """
         Analyses the graph for the number of nodes, number of odd, even nodes,
-        whether it's an Eulerian or semi-Eulerian path or invalid etc.
+        whether it's Eulerian, semi-Eulerian or invalid.
 
         :return:
         """
@@ -81,7 +113,7 @@ class Graph:
         else:
             graph_type = "Invalid graph type"
 
-        print(f"Nodes         : {self.nodes}      ({'Even' if self.nodes % 2 == 0 else 'Odd'})")
+        print(f"Nodes         : {self.nodes}      ({'Even' if self.node_count % 2 == 0 else 'Odd'})")
         print(f"Odd nodes     : {', '.join(self.odd_nodes)}   (Possible starting nodes)")
         print(f"Graph type    : {graph_type}")
 
@@ -105,6 +137,11 @@ class Graph:
             del self.paths[end][self.paths[end].index(start)]
         except KeyError:
             raise KeyError(f"Nodes {start} and {end} are not linked.")
+
+    def get_node(self, label: str):
+        for node in self.nodes:
+            if label == node.label:
+                return node
 
     def node_links(self, node=None):
         if node is None:
