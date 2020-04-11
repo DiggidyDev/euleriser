417837cbf2576f2d4290f35b14474907f8ed295b
Val-PC <valentinewilson@gmail.com>
2020-04-11 01:50:56 +0100

    Added position handling and interface integration

--- node.py
+++ node.py
@@ -1,12 +1,42 @@
 class Node:
 
-    def __init__(self, label: 'Node ID' = None):
-        self.label = label
-        self.position = None
+    def __init__(self, identifier: int, interface: "Interface object"):
         self.connections = []
+        self.identifier = identifier
+        self.interface = interface
+        self.centre = None
+        self.radius = None
+        self.visits = 0
 
-    def __str__(self):
+    def __int__(self):
+        return int(self.identifier)
+
+    @property
+    def border_positions(self):
+        return (self.centre[0] - self.radius, self.centre[1] - self.radius), (self.centre[0] + self.radius, self.centre[1] + self.radius)
+
+    def connect_to(self, node: "Node object"):
+        if node.identifier not in self.connections:
+            self.connections.append(node.identifier)
+        else:
+            raise PermissionError(f"Path connection already exists between nodes {self.identifier} and {node.identifier}")
+
+    def disconnect_from(self, node: "Node object"):
+        if node.identifier in self.connections:
+            self.connections.pop(self.connections.index(node))
+        else:
+            raise PermissionError(f"No initial path connection found between nodes {self.identifier} and {node.identifier}")
+
+    def set_position(self, x, y, r):
+        """
+        Sets the centre position and radius of the node.
+
+        :param x:
+        :param y:
+        :param r:
+        :return:
+        """
+        self.centre = (x, y)
+        self.radius = r
+
         return
-
-    def connect_to(self, label: "Node"):
-        self.connections.append(label)
